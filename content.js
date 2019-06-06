350149f8d4489bacb1f8cec771090f11488df9a5
James Clarke <jamesraymondclarke@gmail.com>
2019-06-06 16:32:18 +0100

    LOL
    because alex cannot code
    
    writing kickass code

--- content.js
+++ content.js
@@ -3,18 +3,19 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
       var firstHref = "text";
-  	  var textAsVoice = new SpeechSynthesisUtterance(getSelectionText());
-  	  window.speechSynthesis.speak(textAsVoice);
+      var textAsVoice = new SpeechSynthesisUtterance(getSelectionText() + " also, alex cannot code to save his bike");
+      // textAsVoice.pitch = 2;
+      window.speechSynthesis.speak(textAsVoice);
     }
   }
 );
 
 function getSelectionText() {
-    if (window.getSelection) {
-        return window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        return document.selection.createRange().text;
-    } else {
-    	return "Nothing highlighted."
-    }
+  if (window.getSelection) {
+    return window.getSelection().toString();;
+  } else if (document.selection && document.selection.type != "Control") {
+    return document.selection.createRange().text;
+  } else {
+    return "Nothing highlighted."
+  }
 }
